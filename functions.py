2b363d528aff5a165390e8b996fa95f1d89e578e
francisco andrade <frma059@gmail.com>
2022-05-24 16:01:20 -0300

    Change capital update method from for loops to numpy updating via logical masking.

--- functions.py
+++ functions.py
@@ -5,60 +5,90 @@ from numpy.random import default_rng
 from sklearn.preprocessing import scale
 
 
-def population(size,lower_bound,upper_bound,mean,std):
-    
-    #size: number of individuals in the population
+def population(size, lower_bound, upper_bound, mean, std):
 
-    #lb: lower bound of talent value
-    lb = np.full(size,lower_bound)
+    # size: number of individuals in the population
 
-    #ub: upper bound of talent value
-    ub = np.full(size,upper_bound)
+    # lb: array containing the lower bound of talent value
+    # lb = np.full(size, lower_bound)
 
-    #mu: mean of talent distribution
-    mu = np.full(size,mean)
-   
-    #sigma: standard deviation of talent distribution
-    sigma = np.full(size,std)
+    # ub: array containing the upper bound of talent value
+    # ub = np.full(size, upper_bound)
+
+    # mu: array containing the mean of talent distribution
+    # mu = np.full(size, mean)
+
+    # stdev: array containing the standard deviation of talent distribution
+    # stdev = np.full(size, std)
 
     talent = np.zeros(size)
-    talent = sp.stats.truncnorm.rvs((lb-mu)/sigma, (ub-mu)/sigma, loc=mean, scale=std) 
+
+    talent = sp.stats.truncnorm.rvs((lower_bound - mean) / std,
+                                    (upper_bound - mean) / std,
+                                    loc=mean,
+                                    scale=std,
+                                    size=size)
+
+    # talent = np.random.default_rng().normal(mean, std, size=size)
 
     mean = np.mean(talent)
     std = np.std(talent)
-    
-    talent_sort = np.sort(talent,0,'quicksort')
-    talent_index = np.argsort(talent,0,'quicksort')
+
+    talent_sort = np.sort(talent, 0, 'quicksort')
+    talent_index = np.argsort(talent, 0, 'quicksort')
 
     return talent_sort, talent_index
 
 
-def evolution(talent,time,unlucky_event,lucky_event):
+def evolution(talent, time, unlucky_event, lucky_event):
 
     rng = default_rng()
 
-    capital = np.full((len(talent),time),10.0)
+    capital = np.zeros((len(talent), time))
 
-    for i in range(time-1):
-        for j in range(len(talent)):
+    # Initializing all individuals to a starting capital of 10.0:
 
-            a = rng.uniform(0.0,1.0)
+    np.place(capital[:, 0], mask=np.zeros(len(talent)) == 0, vals=10.0)
 
-            if a <=unlucky_event:
+    for i in range(time - 1):
+        a = rng.uniform(0.0, 1.0, size=len(talent))
+        b = rng.uniform(0.0, 1.0, size=len(talent))
 
-                capital[j,i+1] = capital[j,i]/2
-            
-            elif a<= unlucky_event + lucky_event:
-                
-                b = rng.uniform(0.0,1.0)
+        arr_source = capital[:, i]
 
-                if b<talent[j]:
-                    capital[j,i+1] = capital[j,i]*2
+        # Upadting capital of those who went through unlucky events:
 
-                else:
-                    capital[j,i+1] = capital[j,i]
+        unlucky_mask = a < unlucky_event
+        unlucky_vals = np.extract(unlucky_mask, arr_source)
 
-            else:
-                capital[j,i+1] = capital[j,i]
+        np.place(capital[:, i + 1], mask=unlucky_mask, vals=unlucky_vals / 2)
+
+        # Updating capital of those who went through lucky events AND managed to capitalize:
+
+        lucky_mask = (a >= unlucky_event) & (a < unlucky_event + lucky_event) & (b < talent)
+        lucky_vals = np.extract(lucky_mask, arr_source)
+
+        np.place(capital[:, i + 1], mask=lucky_mask, vals=lucky_vals * 2)
+
+        # Upadting capital of those who didn't go through any events OR failed to capitalize:
+
+        neutral_mask = ((a >= unlucky_event + lucky_event) |
+                        ((a >= unlucky_event) &
+                        (a < unlucky_event + lucky_event) &
+                        (b > talent))
+                        )
+        neutral_vals = np.extract(neutral_mask, arr_source)
+
+        np.place(capital[:, i + 1], mask=neutral_mask, vals=neutral_vals)
+
+        # Checking if the masks cover the entirey of the source array:
+
+        full_mask = np.array((unlucky_mask, lucky_mask, neutral_mask))
+        check = np.logical_or.reduce(full_mask)
+
+        if np.all(check):
+            pass
+        else:
+            raise ValueError('Failure to update capital')
 
     return capital
--- main.py
+++ main.py
@@ -2,21 +2,59 @@ from time import time
 import functions as f
 from matplotlib import pyplot as plt
 import numpy as np
-iterations = 80
 
-n = 1000
+np.set_printoptions(precision=3)
+
+# iter_n: number of iterations to go through
+
+iter_n = 80
+
+# pop_n: number of individuals in the popoulation
+
+pop_n = 1000
+
+# lb: lower bound of talent
+# ub: upper bound of talent
 
 lb, ub = 0, 1
 
-mu, sigma = 0.6, 0.1
+# mu: average value of the talent distribution
+# std: standard deviation of the talent distribution
 
-talent, t_i = f.population(n, lb, ub, mu, sigma)
+mu, std = 0.6, 0.1
 
-cap = f.evolution(talent, iterations, 0.03, 0.03)
+# Creating a population with the desired parameters for talent distribution:
 
-final_cap = cap[:, iterations - 1]
+    # talent: array containing sorted values of talent
+
+    # t_i: array containing the indices to the unsorted talent array, i.e:
+
+        # talent[t_i[0]] is the talent of the first individual of the population
+        # talent[t_i[j-1]] is the talent of the jth individual of the population
+        # talent[t_i[-1]] is the talent of the last individual of the population
+
+talent, t_i = f.population(pop_n, lb, ub, mu, std)
+
+plt.hist(talent, bins=50)
+plt.title('Histogram of talent distribution')
+plt.show()
+
+# le: chance for an individual to go through a lucky event
+le = 0.03
+
+# ue: chance for an individual to go through an unlucky event
+ue = 0.03
+
+cap = f.evolution(talent, iter_n, ue, le)
+
+final_cap = cap[:, iter_n - 1]
+
+plt.hist(final_cap, bins=50)
+plt.title('Histogram of the final capital')
+plt.show()
 
 plt.plot(talent, final_cap)
+plt.title('Final capital vs talent')
 plt.show()
 
 print(np.max(final_cap), talent[np.argmax(final_cap)])
