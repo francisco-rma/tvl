d599134ae24a18b33b1abe6179d7a97dc4c12965
francisco andrade <frma059@gmail.com>
2022-05-30 17:40:41 -0300

    add (expected value of final position) x (talent) to animation

--- anim_test.py
+++ anim_test.py
@@ -5,7 +5,7 @@ import numpy as np
 np.set_printoptions(precision=3)
 
 # iter_n: number of iterations to go through
-iter_n = 80
+iter_n = 500
 
 # pop_n: number of individuals in the popoulation
 pop_n = 1000
@@ -19,13 +19,10 @@ lb, ub = 0, 1
 mu, std = 0.6, 0.1
 
 # le: chance for an individual to go through a lucky event
-le = 0.03
+le = 0.4
 
 # ue: chance for an individual to go through an unlucky event
-ue = 0.03
-
-# runs: number of runs to aggregate over
-runs = 10000
+ue = 0.4
 
 talent, t_i = f.populate(pop_n, lb, ub, mu, std)
 
@@ -34,19 +31,22 @@ positions = f.evolution(talent, iter_n, ue, le, history=True)
 fig1, ax1 = plt.subplots()
 
 ax1.set_ylim(np.min(positions), np.max(positions))
-ax1.set_xlim(talent.min(), talent.max())
+ax1.set_xlim(np.min(talent), np.max(talent))
 ax1.set_title('Position distribution')
 ax1.set_xlabel('Talent')
 ax1.set_ylabel('Position')
 
-bar = ax1.bar(np.linspace(0, 1, num=1000), positions[:, 0], width=0.001)
+bar = ax1.bar(np.linspace(0, np.max(talent), num=1000), positions[:, 0], width=0.001)
+
+line1, = ax1.plot(talent, -0 * le * (np.ones((pop_n)) - talent), color='red')
 
 def bar_animate(i):
     for b, h in zip(bar, positions[:, i]):
         b.set_height(h)
+    line1.set_ydata(-i * le * (np.ones((pop_n)) - talent))
     return
 
 
-ani2 = animation.FuncAnimation(fig1, bar_animate, frames=iter_n + 1, blit=True, repeat=False)
+ani1 = animation.FuncAnimation(fig1, bar_animate, frames=iter_n + 1, blit=True, repeat=False, interval=1)
 
 plt.show()
